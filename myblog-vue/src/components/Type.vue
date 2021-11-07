e99bc347a9611743a68a66a6f980c81f2aef6554
shuai <liershuai.cn@gmail.com>
2021-11-07 22:50:11 +0800

    分类页面

--- myblog-vue/src/components/Type.vue
+++ myblog-vue/src/components/Type.vue
@@ -1,29 +1,86 @@
 <template>
-  <h1 class="a">分类</h1><br/><br/><br/>
-  <h1 class="a">分类</h1><br/><br/><br/>
-  <h1 class="a">分类</h1><br/><br/><br/>
-  <h1 class="a">分类</h1><br/><br/><br/>
-  <h1 class="a">分类</h1><br/><br/><br/>
-  <h1 class="a">分类</h1><br/><br/><br/>
-  <h1 class="a">分类</h1><br/><br/><br/>
-  <h1 class="a">分类</h1><br/><br/><br/>
-  <h1 class="a">分类</h1><br/><br/><br/>
-  <h1 class="a">分类</h1><br/><br/><br/>
-  <h1 class="a">分类</h1><br/><br/><br/>
-  <h1 class="a">分类</h1><br/><br/><br/>
-  <h1 class="a">分类</h1><br/><br/><br/>
-  <h1 class="a">分类</h1><br/><br/><br/>
-  <h1 class="a">分类</h1><br/><br/><br/>
+  <!-- 标签页-头部 -->
+  <div class="types">
+    <div class="message">
+      <div class="message_left">分类</div>
+      <div class="message_right">
+        一共<span class="right_number">1234</span>个
+      </div>
+    </div>
+    <div class="labels">
+      <a class="label" href="#">Java</a>
+      <a class="label" href="#">css</a>
+      <a class="label" href="#">Docker</a>
+      <a class="label" href="#">其他</a>
+      <a class="label" href="#">生活</a>
+      <a class="label" href="#">学习</a>
+      <a class="label" href="#">Java</a>
+    </div>
+  </div>
+  <!-- 标签页-主体 -->
+  <blog-list></blog-list>
 </template>
 
 <script>
+import BlogList from "@/components/common/BlogList";
 
 export default {
   name: "Types",
-  components: {}
+  components: {BlogList}
 }
 </script>
 
 <style scoped>
+
+.types {
+  background-color: rgb(255, 255, 255, 0.7);
+  width: 60%;
+  margin: 10px auto 0;
+  height: 150px;
+}
+
+.message {
+  height: 20%;
+  width: 100%;
+  border-bottom-style: solid;
+  border-bottom-width: 2px;
+}
+
+.message_left {
+  float: left;
+  margin: 5px;
+  font-size: 20px;
+  font-weight: bold;
+  font-family: "Lucida Calligraphy", cursive, serif, sans-serif;
+}
+
+.message_right {
+  float: right;
+  margin: 4px;
+}
+
+.right_number {
+  font-size: 20px;
+  color: #ffaa7f;
+}
+
+.labels {
+  clear: both;
+}
+
+.label {
+  display: inline-block;
+  float: left;
+  border-style: solid;
+  border-width: 2px;
+  border-radius: 10px;
+  width: fit-content;
+  height: 30px;
+  margin: 10px;
+  padding-left: 10px;
+  padding-right: 10px;
+  line-height: 30px;
+  text-decoration: none;
+}
 
 </style>
